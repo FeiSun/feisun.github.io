b7354a434082539c2bad7be2a7e8826663e7b309
FeiSun <ofey.sunfei@gmail.com>
2025-01-12 06:23:13 +0000

    Deploying to gh-pages from @ FeiSun/feisun.github.io@4bdb4ced2f450921f477e8935613afbfffd4cd4f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/workshop/";
           },
+        },{id: "nav-news",
+          title: "News",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
