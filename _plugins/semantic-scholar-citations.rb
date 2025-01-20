ef8138e638784185035b876f486fb8d877e6dc3a
Ofey <ofey.sunfei@gmail.com>
2025-01-20 17:18:27 +0800

    fix semantic scholar bug

--- _plugins/semantic-scholar-citations.rb
+++ _plugins/semantic-scholar-citations.rb
@@ -1,5 +1,3 @@
-# _plugins/semantic_scholar_citations.rb
-
 require "active_support/all"
 require "json"
 require "open-uri"
@@ -28,7 +26,7 @@ module Jekyll
       citation_count = 0
       max_retries = 10
       api_url = "https://api.semanticscholar.org/graph/v1/paper/#{paper_id}?fields=influentialCitationCount"
-      puts "[Debug] API URL: #{api_url}"
+      # puts "[Debug] API URL: #{api_url}"
 
       max_retries.times do |attempt|
         begin
@@ -37,13 +35,13 @@ module Jekyll
 
           # Fetch the JSON response
           response = URI.open(api_url, "User-Agent" => "Ruby/#{RUBY_VERSION}").read
-          puts "[Debug] Raw API Response: #{response}"
+          # puts "[Debug] Raw API Response: #{response}"
           data = JSON.parse(response)
-          puts "[Debug] Parsed JSON: #{data.inspect}"
+          # puts "[Debug] Parsed JSON: #{data.inspect}"
 
           # Extract influential citation count
           citation_count_raw = data["influentialCitationCount"] || 0
-          puts "[Debug] raw citation count: #{citation_count_raw}"
+          # puts "[Debug] raw citation count: #{citation_count_raw}"
 
           citation_count = Helpers.number_to_human(
             citation_count_raw,
@@ -55,7 +53,7 @@ module Jekyll
               billion: "B"
             }
           )
-          puts "[Debug] Formatted citation count: #{citation_count}"
+          # puts "[Debug] Formatted citation count: #{citation_count}"
 
           # Exit the retry loop if successful
           break
@@ -66,11 +64,7 @@ module Jekyll
       end
 
       # Return the result
-      # final_value = citation_count.to_s.strip.empty? ? "N/A" : citation_count.to_s.strip
-      # puts "Debug - Final citation count for #{paper_id}: '#{final_value}'"
-      # final_value
-      puts "[Debug] Final return value: '#{citation_count}'"
-
+      # puts "[Debug] Final return value: '#{citation_count}'"
       return "#{citation_count}"
     end
   end
